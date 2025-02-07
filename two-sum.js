d4d11bd6280577cb3c2f2c470c1cb37e1c14b675
Vishal <160380978+vishalkumar2024@users.noreply.github.com>
2025-02-07 14:38:47 +0530

    Update two-sum.js

--- two-sum.js
+++ two-sum.js
@@ -13,19 +13,19 @@
 
 //--------  Two pointer approach  ---------//
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8];
-// let target = 8;
+let arr = [1, 2, 3, 4, 5, 6, 7, 8];
+let target = 8;
 
-// let i = 0;
-// let j = arr.length - 1;
-// while (i <= j) {
-//     if (arr[i] + arr[j] == target) {
-//         console.log(arr[i], arr[j]);
-//         break;
-//     }
-//     else if (arr[i] + arr[j] > target) j--
-//     else if (arr[i] + arr[j] < target) i++
-// }
+let i = 0;
+let j = arr.length - 1;
+while (i <= j) {
+    if (arr[i] + arr[j] == target) {
+        console.log(arr[i], arr[j]);
+        break;
+    }
+    else if (arr[i] + arr[j] > target) j--
+    else if (arr[i] + arr[j] < target) i++
+}
 
 // Using for loop//
 
