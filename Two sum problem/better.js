9b2390096027eea176e64198e0167f518ad20a27
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-02-10 12:24:29 +0530

    added

--- Two sum problem/better.js
+++ Two sum problem/better.js
@@ -1,20 +1,24 @@
-
-
 //--------  Two pointer approach  ---------//
 
+function twoSum() {
+    let i = 0;
+    let j = arr.length - 1;
+    let res = [];
+    while (i <= j) {
+        if (arr[i] + arr[j] == target) {
+            res.push(arr[i], arr[j]);
+            break;
+        }
+        else if (arr[i] + arr[j] > target) j--
+        else if (arr[i] + arr[j] < target) i++
+    }
+    return res
+}
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let target = 8;
+console.log(twoSum(arr,target));
 
-let i = 0;
-let j = arr.length - 1;
-while (i <= j) {
-    if (arr[i] + arr[j] == target) {
-        console.log(arr[i], arr[j]);
-        break;
-    }
-    else if (arr[i] + arr[j] > target) j--
-    else if (arr[i] + arr[j] < target) i++
-}
+
 
 // Using for loop//
 
