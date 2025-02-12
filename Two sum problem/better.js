482c2b20027247b0017a20e1fc3efde8d724e54d
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-02-12 23:16:20 +0530

    modified

--- Two sum problem/better.js
+++ Two sum problem/better.js
@@ -2,7 +2,7 @@
 
 //constraint is that Array should be sorted
 
-function twoSum(arr,target) {
+function twoSum(arr, target) {
     let i = 0;
     let j = arr.length - 1;
     let res = [];
@@ -18,17 +18,25 @@ function twoSum(arr,target) {
 }
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let target = 8;
-console.log(twoSum(arr,target));
+console.log(twoSum(arr, target));
 
 
 
 // Using for loop//
 
-// for (let i = 0, j = arr.length - 1; i <= j;) {
-//     if (arr[i] + arr[j] == target) {
-//         console.log(arr[i], arr[j]);
-//         break;
+// function twoSum(arr, target) {
+//     let res=[];
+//     for (let i = 0, j = arr.length - 1; i <= j;) {
+//         if (arr[i] + arr[j] == target) {
+//             res.push(arr[i], arr[j]);
+//             break;
+//         }
+//         else if (arr[i] + arr[j] > target) j--
+//         else if (arr[i] + arr[j] < target) i++
 //     }
-//     else if (arr[i] + arr[j] > target) j--
-//     else if (arr[i] + arr[j] < target) i++
+//     return res
 // }
+
+// let arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
+// let target=8;
+// console.log(twoSum(arr1, target));
