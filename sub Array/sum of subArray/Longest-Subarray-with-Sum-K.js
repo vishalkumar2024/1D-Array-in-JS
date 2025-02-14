8f2940857aca95fdea085ab35f31988266ee3033
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-02-14 09:41:26 +0530

    modified

--- sub Array/sum of subArray/Longest-Subarray-with-Sum-K.js
+++ sub Array/sum of subArray/Longest-Subarray-with-Sum-K.js
@@ -1,15 +1,17 @@
-let arr = [10, 5, 2, 7, 1, -10]
-let target = 15;
 
-let maxArray = 0;
-for (let i = 0; i < arr.length; i++) {
-    let sum = 0;
-    for (let j = i; j < arr.length; j++) {
-        sum += arr[j];
-        if (sum == target) {
-            maxArray = Math.max(maxArray, j - i + 1);
+function maxSubArray(arr,target) {
+    let maxArray = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let sum = 0;
+        for (let j = i; j < arr.length; j++) {
+            sum += arr[j];
+            if (sum == target) {
+                maxArray = Math.max(maxArray, j - i + 1);
+            }
         }
     }
+    return maxArray;
 }
-
-console.log(maxArray);
+let arr = [10, 5, 2, 7, 1, -10]
+let target = 15;
+console.log(maxSubArray(arr,target));
