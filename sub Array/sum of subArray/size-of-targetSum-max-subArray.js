740e2feec6fc72d2a5ddd93dfa7e752b98cb8a90
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-02-14 09:54:04 +0530

    added

--- sub Array/sum of subArray/size-of-targetSum-max-subArray.js
+++ sub Array/sum of subArray/size-of-targetSum-max-subArray.js
@@ -1,19 +1,5 @@
-// let arr = [1, 8, -3, -4, 5, -6];
-// let maxSum=Number.MIN_VALUE;
-// let target=5;
-// for (let i = 0; i < arr.length; i++) {
-//     let sum=0;
-//     for (let j = i; j < arr.length; j++) {
-//         sum+=arr[j];
-//         maxSum=Math.max(sum,maxSum);
-//     }
-// }
-// console.log(maxSum);
-
-// TC=O(n*n)
-// SC=O(1)
-
-
+// This function returns the size of that largest subArray whose sum is 
+// equals to target.
 
 function subarraySum(arr, target) {
     let i = 0, j = 0;
