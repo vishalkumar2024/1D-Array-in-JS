80bf2f1212c388a8db48863afad2809fa4807fa7
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-05 11:37:25 +0530

    added one file

--- palindrome.js
+++ palindrome.js
@@ -5,39 +5,14 @@ var isPalindrome = function (x) {
     if (x < 0) {
         return false;
     }
-    
         while (x > 0) {
             lastDigit = x % 10;
             reverse = reverse * 10 + lastDigit;
             x = Math.floor(x / 10);
         }
-        if (reverse === originalNum) return true;
-        else return false;
+
+        return reverse===originalNum?true:false
  
 };
-console.log(isPalindrome(202));
+console.log(isPalindrome(2092));
 
-// function isPalindrome(x) {
-//     // Negative numbers are not palindromes
-//     if (x < 0) {
-//         return false;
-//     }
-
-//     let reversed = 0;
-//     let temp = x;
-
-//     // Reverse the number
-//     while (temp !== 0) {
-//         let digit = temp % 10; // Extract the last digit
-//         reversed = reversed * 10 + digit; // Build the reversed number
-//         temp = Math.floor(temp / 10); // Remove the last digit
-//     }
-
-//     // Check if the original number is equal to the reversed number
-//     return reversed === x;
-// }
-
-// // Example usage
-// console.log(isPalindrome(121)); // true
-// console.log(isPalindrome(-121)); // false
-// console.log(isPalindrome(10)); // false
