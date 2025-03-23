7cc3678969e01a9983009269ec17344a3b285e7f
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-23 09:07:30 +0530

    added some modification on better.js in 2sum

--- 2-sum problem/better.js
+++ 2-sum problem/better.js
@@ -5,16 +5,13 @@
 function twoSum(arr, target) {
     let i = 0;
     let j = arr.length - 1;
-    let res = [];
     while (i <= j) {
         if (arr[i] + arr[j] == target) {
-            res.push(arr[i], arr[j]);
-            break;
+            return [i, j];
         }
         else if (arr[i] + arr[j] > target) j--
-        else if (arr[i] + arr[j] < target) i++
+        else i++
     }
-    return res;
 }
 let arr = [1, 2, 3, 4, 5, 6, 7, 8];
 let target = 8;
@@ -25,16 +22,13 @@ console.log(twoSum(arr, target));
 // Using for loop//
 
 // function twoSum(arr, target) {
-//     let res=[];
 //     for (let i = 0, j = arr.length - 1; i <= j;) {
 //         if (arr[i] + arr[j] == target) {
-//             res.push(arr[i], arr[j]);
-//             break;
+//             return [i,j];
 //         }
 //         else if (arr[i] + arr[j] > target) j--
-//         else if (arr[i] + arr[j] < target) i++
+//         else i++
 //     }
-//     return res;
 // }
 
 // let arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
