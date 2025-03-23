078eba78200157dcaeaae643c2371fbf806f57ae
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-23 10:42:27 +0530

    added some modification

--- Best Time to Buy and Sell Stock/brute-force.js
+++ Best Time to Buy and Sell Stock/brute-force.js
@@ -1,18 +1,18 @@
 // Leetcode 121 Question
 
 function maxProfit(arr) {
-    let sum = 0
+    let diff = 0
     for (let i = 0; i < arr.length; i++) {
         for (let j = i; j < arr.length; j++) {
-            if (arr[j] - arr[i] > sum) {
-                sum = arr[j] - arr[i]
+            if (arr[j] - arr[i] > diff) {
+                diff = arr[j] - arr[i]
             }
         }
     }
-    return sum;
+    return diff;
 }
 let arr = [7,1,5,3,6,4];
-console.log(maxProfit(arr))
+console.log(maxProfit(arr));
 
 // TC=O(n*n)
 // SC=O(1)
