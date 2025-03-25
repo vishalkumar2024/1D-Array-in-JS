f956bfc7fd0eccd88231139182c3a263c874d10a
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-25 12:59:31 +0530

    modify num appear ones

--- num-appears-ones/better.js
+++ num-appears-ones/better.js
@@ -1,22 +1,21 @@
-let arr=[4,1,2,1,2];
-
-let obj={};
-
-for(let i=0;i<arr.length;i++){
-    if(obj[arr[i]]==undefined){
-        obj[arr[i]]=1;
+function appearOnes(arr) {
+    let obj = {};
+    for (let i = 0; i < arr.length; i++) {
+        if (obj[arr[i]] == undefined) {
+            obj[arr[i]] = 1;
+        }
+        else {
+            obj[arr[i]]++
+        }
     }
-    else {
-        obj[arr[i]]++
-    }
-}
-console.log(obj);
 
-for(let key in obj){
-    if(obj[key]==1){
-        console.log(key);
+    for (let key in obj) {
+        if (obj[key] == 1) {
+            return key;
+        }
     }
 }
-
+let arr = [4, 1, 2, 1, 2];
+console.log(appearOnes(arr));
 // TC=O(n+n)
 // SC=O(n)
--- num-appears-ones/brute-force.js
+++ num-appears-ones/brute-force.js
@@ -1,17 +1,33 @@
-let arr = [4, 1, 2, 1, 4, 7, 2];
-let count = 0;
-for (let i = 0; i < arr.length; i++) {
-    let count = 0;
-    let res = arr[i];
-    for (let j = 0; j < arr.length; j++) {
-        if (arr[j] == res) {
-            count++;
+function appearOnes(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let count = 0;
+        let res = arr[i];
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] == res) {
+                count++;
+            }
+        }
+        if (count == 1) {
+            return res;
         }
     }
-    if (count == 1) {
-        console.log(res)
-    }
+
 }
+
+// function appearOnes(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         let res=false;
+//         for (let j = 0; j < arr.length; j++) {
+//             if(arr[i]==res){
+//                 res=true;
+//                 break;
+//             }
+//         }
+//     }
+// }
+let arr = [4, 1, 2, 1, 4, 7, 2];
+console.log(appearOnes(arr));
+
 
 // TC=O(n*n)
 // SC=O(1)
