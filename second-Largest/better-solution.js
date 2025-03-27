f6a56d907f9a28bba6f2d9f7d263d8c957e073f5
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-27 12:31:46 +0530

    modify second large folder

--- second-Largest/better-solution.js
+++ second-Largest/better-solution.js
@@ -1,18 +1,20 @@
+function sLarge(arr) {
+    let large = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > large) {
+            large = arr[i];
+        }
+    }
+
+    let secondLarge = -1;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= secondLarge && arr[i] != large) {
+            secondLarge = arr[i];
+        }
+    }
+    return secondLarge;
+}
 let arr = [3, 5, 3, 6, 1, 5];
-
-let large = arr[0];
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > large) {
-        large = arr[i];
-    }
-}
-console.log(large)
-let sLarge = -1;
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] >= sLarge && arr[i] != large) {
-        sLarge = arr[i];
-    }
-}
-console.log(sLarge);
-
-// Time Complexity= o(2n)
+console.log(sLarge(arr));
+// TC=O(n+n)
+// SC=O(1)
--- second-Largest/optimal-solution.js
+++ second-Largest/optimal-solution.js
@@ -4,7 +4,7 @@ function secondLarge() {
     let large = arr[0];
     let sLarge = -1;
 
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < arr.length; i++) { //TC=O(n)
         if (arr[i] > large) {
             sLarge = large;
             large = arr[i];
@@ -18,3 +18,6 @@ function secondLarge() {
 }
 let arr = [12, 5, 3, 8, 5];
 console.log(secondLarge(arr));
+
+// TC=O(n)
+// SC=O(1)
