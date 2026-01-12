044b163b0959d0effd8af2462a89304101238ce5
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-01-12 09:30:55 +0530

    inprove count-number file

--- Count-number.js
+++ Count-number.js
@@ -5,11 +5,13 @@ function getCount(arr, num1, num2) {
     for(let i=0;i<arr.length;i++){
         if(arr[i]===num1){
             startIdx=i
+            break
         }
     }
     for(let i=arr.length-1;i>=0;i--){
         if(arr[i]===num2){
-            endIdx=(arr.length-1)-i;
+            endIdx=i;
+            break
         }
     }
     if(startIdx===endIdx) return 0;
@@ -21,7 +23,7 @@ function getCount(arr, num1, num2) {
         return (endIdx-startIdx)-1
     }
 }
-let arr=[3,7,6,7,1,8,8,18];
+let arr=[3,7,6,17,1,8,7,18];
 let num1=7;
 let num2=7; 
 console.log(getCount(arr,num1,num2));
